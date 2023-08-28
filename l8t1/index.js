ea4f2e8f4779b18f608e3472b9ad53f1110a3c6e
sayHelloWorld <sayHelloWorld@users.noreply.github.com>
2023-08-28 19:46:26 +1000

    save

---
+++ l8t1/index.js
@@ -0,0 +1,8 @@
+//http请求方法
+//url.length has limit -- could lead to error 
+//GET query     example.com/articles/123?limit=20&name=ben&age=30 (filter )
+//POST          no cache, safe. large size. 
+//params        example.com/articles/123 (123:param, unique)
+//PUT           send to server and /or create new
+//body          
+
