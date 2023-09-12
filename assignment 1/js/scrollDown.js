33e57811c4128c5f2353557f12c1efeb90e346d9
sayHelloWorld <sayHelloWorld@users.noreply.github.com>
2023-09-12 12:50:30 +1000

    save

--- assignment 1/js/scrollDown.js
+++ assignment 1/js/scrollDown.js
@@ -1,26 +1,41 @@
-//dynamicly obtain the position of a container
-
-if(id1){ const id1 = document.getElementById(id1)}
 
 
-
-
-
-window.addEventListener('scroll', function() {
-    var scrollPosition = window.scrollY;
-
-    // Define the scroll positions where you want to change the background image
-    var position1 = 500; // Change this to your desired position
-    var position2 = 1000; // Change this to your desired position
-
-    if (scrollPosition < position1) {
-        // Change background image to the first image
-        document.querySelector('.container').style.backgroundImage = "url('image1.jpg')";
-    } else if (scrollPosition < position2) {
-        // Change background image to the second image
-        document.querySelector('.container').style.backgroundImage = "url('image2.jpg')";
+// trigger this function every time the user scrolls
+window.onscroll = function (event) {
+    var scroll = window.scrollY;
+    if (scroll < 300) {
+      document.querySelector(".main-body__static-bkg1").style.opacity  = '0.75';
+      document.querySelector(".main-body__static-bkg2").style.opacity  = '0';
+      document.querySelector(".main-body__static-bkg3").style.opacity  = '0';
+    } else if (scroll >= 300 && scroll < 600) {
+      document.querySelector(".main-body__static-bkg1").style.opacity  = '0';
+      document.querySelector(".main-body__static-bkg2").style.opacity  = '0';
+      document.querySelector(".main-body__static-bkg3").style.opacity  = '0';
+        // yellow
+        // body.style.backgroundColor = 'yellow';
+    } else if (scroll >= 600 && scroll < 1200) {
+      document.querySelector(".main-body__static-bkg1").style.opacity = '0';
+      document.querySelector(".main-body__static-bkg2").style.opacity = '0.75';
+      document.querySelector(".main-body__static-bkg3").style.opacity = '0';
+        // blue
+        // body.style.backgroundColor = 'blue';
+    } else if (scroll >= 1200 && scroll < 1800) {
+      document.querySelector(".main-body__static-bkg1").style.opacity = '0';
+      document.querySelector(".main-body__static-bkg2").style.opacity = '0';
+      document.querySelector(".main-body__static-bkg3").style.opacity = '0';
+        // orange
+        // body.style.backgroundColor = 'orange';
+    } else if (scroll >= 1800 && scroll < 3000) {
+      document.querySelector(".main-body__static-bkg1").style.opacity = '0';
+      document.querySelector(".main-body__static-bkg2").style.opacity = '0';
+      document.querySelector(".main-body__static-bkg3").style.opacity = '0.75';
+        // red
+        // body.style.backgroundColor = 'red';
     } else {
-        // Change background image to the third image or any other as needed
-        document.querySelector('.container').style.backgroundImage = "url('image3.jpg')";
+      document.querySelector(".main-body__static-bkg1").style.opacity = '0';
+      document.querySelector(".main-body__static-bkg2").style.opacity = '0';
+      document.querySelector(".main-body__static-bkg3").style.opacity = '0';
+        // purple
+        // body.style.backgroundColor = 'purple';
     }
-});
+}
